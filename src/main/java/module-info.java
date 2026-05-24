82c5bd12c602e44ebcac84b40a2bccfbfe733633
Ariyan Aftab Spandan <ariyanspandan@gmail.com>
2026-05-24 22:41:30 +0600

    Commit 3: Add Java module descriptor (module-info.java)

---
+++ src/main/java/module-info.java
@@ -0,0 +1,19 @@
+module com.example.rideease {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+    requires com.google.gson;
+    requires java.net.http;
+    requires java.sql;
+
+    opens com.example.rideease to javafx.fxml;
+    exports com.example.rideease;
+}
